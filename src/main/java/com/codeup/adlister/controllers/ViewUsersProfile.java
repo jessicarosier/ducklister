60395b16d427b11f9158ccb26e88ef59d9688587
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-11-14 08:53:26 -0600

    commit

--- src/main/java/com/codeup/adlister/controllers/ViewUsersProfile.java
+++ src/main/java/com/codeup/adlister/controllers/ViewUsersProfile.java
@@ -13,22 +13,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @WebServlet(name = "ViewUsersProfile", urlPatterns = "/userProfile")
 public class ViewUsersProfile extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //sends the user to the profile page of the user they clicked on
 
+        //get the username of the user that was clicked on
         String username = request.getParameter("username");
         String location = request.getParameter("location");
 
-        //get the user data for the user that was clicked on
+        //get the user data from the DB for the user that was clicked on
         User user = DaoFactory.getUsersDao().findByUsername(username);
 
-        //get the ads that belong to the user that was clicked on
+        //get the ads from the DB that belong to the user that was clicked on
         List<Ad> usersads = new ArrayList<>();
         try {
             usersads = DaoFactory.getAdsDao().getUserAds(user);
@@ -38,8 +37,12 @@ public class ViewUsersProfile extends HttpServlet {
 
 
         //send the user to the profile page of the user they clicked on
+
+        //set the user object for the user that was clicked on to the jsp
         request.setAttribute("userToView", user);
+        //if the user clicks on someone else's profile, tell the view profile servlet where they are coming from
         request.setAttribute("location", location);
+        //send the ads that belong to the user that was clicked on to the jsp
         request.setAttribute("usersAds", usersads);
         request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }
